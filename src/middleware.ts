9d707d5648517d8ccff89f637bcf5a2a749aad3a
DIlan Sancho <121054231+dilanSA132@users.noreply.github.com>
2025-01-24 23:34:11 -0600

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { env } from 'process';
 
 export async function middleware(req: NextRequest) {
+
+  if (req.method === 'OPTIONS') {
+    const res = NextResponse.next();
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res;
+  }
+
+  const res = NextResponse.next();
+  res.headers.set('Access-Control-Allow-Origin', 'https://pagina-5al8gc75i-dilans-projects-76ddfd04.vercel.app');
+  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   const session = await getToken({ req, secret: 'jksde7fufsefjhsoiyawedawdngqwdpoqeuqwdnasigdywdawdkajiwgdyuagwyudqw213kanwuyyg' });
   console.log('session', session);  
   if (!session) {
@@ -27,5 +40,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/menu'],
+  matcher: ['/menu', '/api/v1/roles'],
 };
